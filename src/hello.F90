21017089a91933ef6639561ae79de697c8d6a9f5
Aidan <hartnettaidan@gmail.com>
2023-06-02 15:24:27 -0600

    added fizzbuzz

--- src/hello.F90
+++ src/hello.F90
@@ -1,3 +1,15 @@
 program hello
+implicit none
+integer i :: 10
+do i=1,10
+    print *,i
+    if (mod(i,3)==0) then
+        print*,'Fizz!'
+    end if
+    if (mod(i,5)==0) then
+        print*,'Buzz!'
+    end if
+       
     print*,'Hello World!'
+
     end program hello
